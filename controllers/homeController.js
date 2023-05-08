77bd7f1b826857fe0f5029cdd8bcb65ead922ea7
Manohar <manohar20century@gmail.com>
2023-05-08 15:32:48 +0530

    contact update function working successfully

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,27 +3,52 @@ const Contacts = require('../model/contacts');
 module.exports.home = async (req, res) => {
     try {
         const contacts = await Contacts.find()
-        return res.render("home", {
+        res.render("home", {
             title: "Contacts",
             contact_list: contacts,
         });
     } catch (err) {
-        return res.status(400).send(err);
+        res.status(400).send(err);
     }
 };
 
 module.exports.create = async (req, res) => {
     try {
-        const contact = await Contacts.create({
+        await Contacts.create({
             name: req.body.name,
             phone: req.body.phone
         })
-        return res.redirect('/');
-    } catch (err) {
         res.redirect('/');
-        return res.status(400).send(err);
+    } catch (err) {
+        res.status(400).send(err);
     }
 };
+
+module.exports.update = async(req,res)=>{
+    try{
+        await Contacts.findByIdAndUpdate(req.params.id,{
+            name: req.body.name,
+            phone: req.body.phone
+        });
+        return res.redirect('/');
+    }catch(err){
+        res.redirect('/');
+    return res.status(400).send(err);
+    }
+}
+
+module.exports.update_contact = async (req,res)=>{
+try{
+    const contact = await Contacts.findById(req.params.id);
+    return res.render('update',{
+        title : "Update Contact",
+        contact
+    })
+}catch (err) {
+    res.redirect('/');
+    return res.status(400).send(err);
+}
+}
 
 module.exports.delete = async (req,res)=>{
     try{
--- routes/home.js
+++ routes/home.js
@@ -5,5 +5,7 @@ const homeController = require('../controllers/homeController');
 router.get('/',homeController.home);
 router.post('/create_contact',homeController.create);
 router.get('/delete_contact/:id',homeController.delete);
+router.get('/update_contact/:id',homeController.update_contact);
+router.post('/update_contact/:id',homeController.update);
 
 module.exports = router;
